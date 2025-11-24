bb09452c38e799bc5c1370e9e1b40900c73a022e
Nived-Github <nivedthalora@gmail.com>
2025-11-24 08:56:17 +0530

    This is the Same java code

---
+++ demo.java
@@ -0,0 +1,6 @@
+public class demo{
+	public static void main (String [] args){
+	System.out.println ("This is the Demo code");
+		System.out.println ("This is the  2nd  line of print statment in demo code");
+	}
+}
