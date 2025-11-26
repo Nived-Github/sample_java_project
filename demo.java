c993992b730bfd79866ae5fbce4dd035079b2da4
Nived-Github <48906332+Nived-Github@users.noreply.github.com>
2025-11-26 08:04:28 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,7 @@
 public class demo{
 	public static void main (String [] args){
 	System.out.println ("This is the Demo code");
+	System.out.println ("This is the change for POLL SCM ");
 		System.out.println ("This is the  2nd  line of print statment in demo code");
 	}
 }
