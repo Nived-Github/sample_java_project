dce3b28658fa4654398cce79834251bb237879fd
Nived-Github <48906332+Nived-Github@users.noreply.github.com>
2025-11-28 08:47:15 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,8 @@
 public class demo{
 	public static void main (String [] args){
 	System.out.println ("This is the Demo code");
+	System.out.println ("updated line for testing webhook trigger");
+	
 	
 	}
 }
