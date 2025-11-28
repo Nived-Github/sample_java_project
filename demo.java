b3bc71b677989e7f55b4d479210c585cc345ebf5
Nived-Github <48906332+Nived-Github@users.noreply.github.com>
2025-11-28 08:01:39 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,8 +1,7 @@
 public class demo{
 	public static void main (String [] args){
 	System.out.println ("This is the Demo code");
-	System.out.println ("This is the change for POLL SCM ");
-	System.out.println ("Another Commit- withOption POLL SCM");
-		System.out.println ("This line is for checking POLL SCM in Instance");
+	System.out.println ("for checking Webhook is working or not");
+	
 	}
 }
